0164812c6e8fa8f01759f68efc4d01086183ccc8
Nikita <chebanikita@icloud.com>
2023-10-11 16:37:16 +0300

    added some styles for filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,10 +5,14 @@ import brands from 'makes.json';
 import { filterMake, filterPrice } from 'redux/carsRentSlice/actions';
 
 import { useDispatch } from 'react-redux';
+
+import { stylesMake, stylesPrice } from './Filter.styled';
+
 const prices = [];
 for (let index = 1; index < 13; index += 1) {
   const price = index * 10;
   prices.push({ value: price, label: price });
+  console.log(prices);
 }
 
 const makes = brands.map(el => {
@@ -25,40 +29,38 @@ const Filter = () => {
         justifyContent: 'space-between',
         width: '400px',
         marginBottom: '24px',
+        height: '80px',
       }}
     >
-      <div style={{ width: '225px' }}>
-        <Select
-          isClearable={true}
-          placeholder="Enter the text"
-          options={makes}
-          onChange={e => {
-            if (!e) {
-              dispatch(filterMake(''));
-              return;
-            }
+      <Select
+        isClearable={true}
+        placeholder="Enter the text"
+        options={makes}
+        styles={stylesMake}
+        onChange={e => {
+          if (!e) {
+            dispatch(filterMake(''));
+            return;
+          }
 
-            dispatch(filterMake(e.value));
-          }}
-        ></Select>
-      </div>
+          dispatch(filterMake(e.value));
+        }}
+      ></Select>
 
-      <div>
-        <Select
-          styles={{ width: '225px' }}
-          isClearable={true}
-          placeholder="To $"
-          options={prices}
-          onChange={e => {
-            if (!e) {
-              dispatch(filterPrice(1000));
-              return;
-            }
+      <Select
+        styles={stylesPrice}
+        isClearable={true}
+        placeholder="To $"
+        options={prices}
+        onChange={e => {
+          if (!e) {
+            dispatch(filterPrice(1000));
+            return;
+          }
 
-            dispatch(filterPrice(e.value));
-          }}
-        ></Select>
-      </div>
+          dispatch(filterPrice(e.value));
+        }}
+      ></Select>
     </div>
   );
 };
