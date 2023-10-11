48329f491e5796f216a9cdafdb93c824c08167aa
Nikita <chebanikita@icloud.com>
2023-10-11 23:34:00 +0300

    imports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
 import redux from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { ChakraProvider } from '@chakra-ui/react';
+import { App } from 'App';
+import './index.css';
 import { theme } from 'theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
